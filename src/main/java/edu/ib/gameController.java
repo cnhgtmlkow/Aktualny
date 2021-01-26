6630406dc7e2aa43e20e65c70ed3d4056fb98498
MateuszK <mateusz.konopka11@gmail.com>
2021-01-26 22:51:42 +0100

    init

---
+++ src/main/java/edu/ib/gameController.java
@@ -0,0 +1,351 @@
+package edu.ib;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
+public class gameController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private StackPane GamePane;
+
+    @FXML
+    private ImageView gracz;
+
+    @FXML
+    private ImageView NPCsmieszne;
+
+    @FXML
+    private Button btnKartyOczekujace;
+
+    @FXML
+    private Button btnKartaAktywna;
+
+    @FXML
+    private ImageView kartaAktywna;
+
+    @FXML
+    private Button btnKartaNPC1;
+
+    @FXML
+    private ImageView imageKartaNPC1;
+
+    @FXML
+    private Button btnKartaNPC2;
+
+    @FXML
+    private ImageView imageKartaNPC2;
+
+    @FXML
+    private Button btnKartaNPC3;
+
+    @FXML
+    private ImageView imageKartaNPC3;
+
+    @FXML
+    private Button btnKartaNPC4;
+
+    @FXML
+    private ImageView imageKartaNPC4;
+
+    @FXML
+    private Button btnKartaNPC5;
+
+    @FXML
+    private ImageView imageKartaNPC5;
+
+    @FXML
+    private Button btnKartaNPC6;
+
+    @FXML
+    private ImageView imageKartaNPC6;
+
+    @FXML
+    private Button btnKartaNPC7;
+
+    @FXML
+    private ImageView imageKartaNPC7;
+
+    @FXML
+    private Button btnKartaNPC8;
+
+    @FXML
+    private ImageView imageKartaNPC8;
+
+    @FXML
+    private Button btnKartaGracz1;
+
+    @FXML
+    private ImageView imageKartaGracz1;
+
+    @FXML
+    private Button btnKartaGracz2;
+
+    @FXML
+    private ImageView imageKartaGracz2;
+
+    @FXML
+    private Button btnKartaGracz3;
+
+    @FXML
+    private ImageView imageKartaGracz3;
+
+    @FXML
+    private Button btnKartaGracz4;
+
+    @FXML
+    private ImageView imageKartaGracz4;
+
+    @FXML
+    private Button btnKartaGracz5;
+
+    @FXML
+    private ImageView imageKartaGracz5;
+
+    @FXML
+    private Button btnKartaGracz6;
+
+    @FXML
+    private ImageView imageKartaGracz6;
+
+    @FXML
+    private Button btnKartaGracz7;
+
+    @FXML
+    private ImageView imageKartaGracz7;
+
+    @FXML
+    private Button btnKartaGracz8;
+
+    @FXML
+    private ImageView imageKartaGracz8;
+
+    @FXML
+    void KartaNPC1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void KartaNPC2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void KartaNPC3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void KartaNPC4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void KartaNPC5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void KartaNPC6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void KartaNPC7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void KartaNPC8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void imKartaGracz1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaGracz2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaGracz3(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaGracz4(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaGracz5(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaGracz6(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaGracz7(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaGracz8(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaNPC1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaNPC2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void imKartaNPC3(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaNPC4(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaNPC5(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaNPC6(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imKartaNPC7(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imkartaNPC8(MouseEvent event) {
+
+    }
+
+    @FXML
+    void kartaAktywna(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartaGracz1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartaGracz2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartaGracz3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartaGracz4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartaGracz5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartaGracz6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartaGracz7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartaGracz8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kartyOczekujace(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pictureOfCard(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert GamePane != null : "fx:id=\"GamePane\" was not injected: check your FXML file 'Game.fxml'.";
+        assert gracz != null : "fx:id=\"gracz\" was not injected: check your FXML file 'Game.fxml'.";
+        assert NPCsmieszne != null : "fx:id=\"NPCsmieszne\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartyOczekujace != null : "fx:id=\"btnKartyOczekujace\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaAktywna != null : "fx:id=\"btnKartaAktywna\" was not injected: check your FXML file 'Game.fxml'.";
+        assert kartaAktywna != null : "fx:id=\"kartaAktywna\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaNPC1 != null : "fx:id=\"btnKartaNPC1\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaNPC1 != null : "fx:id=\"imageKartaNPC1\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaNPC2 != null : "fx:id=\"btnKartaNPC2\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaNPC2 != null : "fx:id=\"imageKartaNPC2\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaNPC3 != null : "fx:id=\"btnKartaNPC3\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaNPC3 != null : "fx:id=\"imageKartaNPC3\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaNPC4 != null : "fx:id=\"btnKartaNPC4\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaNPC4 != null : "fx:id=\"imageKartaNPC4\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaNPC5 != null : "fx:id=\"btnKartaNPC5\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaNPC5 != null : "fx:id=\"imageKartaNPC5\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaNPC6 != null : "fx:id=\"btnKartaNPC6\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaNPC6 != null : "fx:id=\"imageKartaNPC6\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaNPC7 != null : "fx:id=\"btnKartaNPC7\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaNPC7 != null : "fx:id=\"imageKartaNPC7\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaNPC8 != null : "fx:id=\"btnKartaNPC8\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaNPC8 != null : "fx:id=\"imageKartaNPC8\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaGracz1 != null : "fx:id=\"btnKartaGracz1\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaGracz1 != null : "fx:id=\"imageKartaGracz1\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaGracz2 != null : "fx:id=\"btnKartaGracz2\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaGracz2 != null : "fx:id=\"imageKartaGracz2\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaGracz3 != null : "fx:id=\"btnKartaGracz3\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaGracz3 != null : "fx:id=\"imageKartaGracz3\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaGracz4 != null : "fx:id=\"btnKartaGracz4\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaGracz4 != null : "fx:id=\"imageKartaGracz4\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaGracz5 != null : "fx:id=\"btnKartaGracz5\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaGracz5 != null : "fx:id=\"imageKartaGracz5\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaGracz6 != null : "fx:id=\"btnKartaGracz6\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaGracz6 != null : "fx:id=\"imageKartaGracz6\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaGracz7 != null : "fx:id=\"btnKartaGracz7\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaGracz7 != null : "fx:id=\"imageKartaGracz7\" was not injected: check your FXML file 'Game.fxml'.";
+        assert btnKartaGracz8 != null : "fx:id=\"btnKartaGracz8\" was not injected: check your FXML file 'Game.fxml'.";
+        assert imageKartaGracz8 != null : "fx:id=\"imageKartaGracz8\" was not injected: check your FXML file 'Game.fxml'.";
+
+    }
+}
